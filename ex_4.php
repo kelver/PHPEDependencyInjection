94dea9b110e6470150843a46670359b00ecc0af6
Kelver Machado <kelver_kmv@hotmail.com>
2017-03-11 04:17:01 -0400

    Parametros e testes básicos com pimple

---
+++ ex_4.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kelver
+ * Date: 11/03/17
+ * Time: 03:13
+ */
+require_once __DIR__.'/vendor/autoload.php';
+
+use Pimple\Container;
+
+class TestAdapter
+{
+    public function __construct()
+    {
+        var_dump(TestAdapter::class.'::__construct()');
+    }
+
+    public function RunTest($message)
+    {
+        var_dump($message);
+    }
+}
+
+class Tester
+{
+    public function __construct(TestAdapter $adapter)
+    {
+        $adapter->RunTest('Rodou um teste.');
+    }
+}
+
+$ioc = new Container();
+
+//TestAdapter
+$ioc['ta'] = function($c){
+    return new TestAdapter;
+};
+
+//Tester
+$ioc['tester'] = $ioc->factory(function($c){
+    return new Tester($c['ta']);
+});
+
+$tester = $ioc['tester'];
+$tester2 = $ioc['tester'];
+
+var_dump($tester, $tester2);
---
+++ ex_5.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kelver
+ * Date: 11/03/17
+ * Time: 03:13
+ */
+require_once __DIR__.'/vendor/autoload.php';
+
+use Pimple\Container;
+
+$ioc = new Container();
+
+$ioc['multiplicador'] = 10;
+
+$ioc['soma'] = $ioc->protect(function ($a, $b){
+    return $a+$b;
+});
+
+$sum = $ioc['soma'];
+echo $sum(1,2)*$ioc['multiplicador'];
